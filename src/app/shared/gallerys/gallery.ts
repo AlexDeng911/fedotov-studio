de560757e0c5b74fc1334dbffd2b0ce140d1f13f
AlexDeng911 <pentod@gmail.com>
2023-06-15 17:00:53 +0300

    feat: little reconstruction, nothing important

--- src/app/shared/gallerys/gallery.ts
+++ src/app/shared/gallerys/gallery.ts
@@ -7,32 +7,16 @@ export const galleries: IGallery = {
 	subCategory: 'time',
 	images: [
 		{
-			url: 'src/assets/images/часы DEN/1.jpg',
+			url: 'assets/images/watches/1.jpg',
 			source: '',
 		},
 		{
-			url: 'https://c.dns-shop.ru/thumb/st4/fit/wm/2000/2000/2f06814e52f8c780fa094eb0c8607c97/9e36eded9a34407a9d962a5da5269fea6df2a3467ff103821f5e3d66791ef1b7.jpg',
-			source: '9e36eded9a34407a9d962a5da5269fea6df2a3467ff103821f5e3d66791ef1b7.jpg',
+			url: 'assets/images/watches/2.jpg',
+			source: '',
 		},
 		{
-			url: 'https://c.dns-shop.ru/thumb/st4/fit/wm/2000/2000/71b76ea354e44f1db0fe5eb21b1aa7d0/5b7d7d6fabaa7ed681c3aa9b050e1d29667c5e65fae923d932b480265a338a8f.jpg',
-			source: '5b7d7d6fabaa7ed681c3aa9b050e1d29667c5e65fae923d932b480265a338a8f.jpg',
-		},
-		{
-			url: 'https://c.dns-shop.ru/thumb/st1/fit/wm/2000/2000/4158bd2cdb5298bf30147ee158fc3f24/a8a469b0ae9088c5ad0b966aec1b25a445d3900aa17373980cd1de69f51dd376.jpg',
-			source: 'a8a469b0ae9088c5ad0b966aec1b25a445d3900aa17373980cd1de69f51dd376.jpg',
-		},
-		{
-			url: 'https://c.dns-shop.ru/thumb/st4/fit/wm/2000/2000/c7234ce61bc2ed05eaa457d737ff42e5/be0a67c8db96314d0c0166f2522a15f7bff8814751b36c4038c9232e71f71b1a.jpg',
-			source: 'be0a67c8db96314d0c0166f2522a15f7bff8814751b36c4038c9232e71f71b1a.jpg',
-		},
-		{
-			url: 'https://c.dns-shop.ru/thumb/st4/fit/wm/2000/2000/289bb1ee285d64fce2069763d1d0b9e0/4894c23d8b72014e31ca6d4f8e47a03947a01173b53ad3b58d44eeef4a0c96b8.jpg',
-			source: '4894c23d8b72014e31ca6d4f8e47a03947a01173b53ad3b58d44eeef4a0c96b8.jpg',
-		},
-		{
-			url: 'https://c.dns-shop.ru/thumb/st4/fit/wm/2000/2000/3d0a7868eb257a8832e2834ebe2653fe/ca500a30fad392e4c09034d083386bc40084c7fb6ee1a42a6b230f1fde79b10b.jpg',
-			source: 'ca500a30fad392e4c09034d083386bc40084c7fb6ee1a42a6b230f1fde79b10b.jpg',
+			url: 'assets/images/watches/14.jpg',
+			source: '',
 		},
 	],
 };
